a33897fc93ee6fa63286fc46fb83699d17a19e8d
Brian McRae <brian.g.mcrae@gmail.com>
2017-11-13 22:40:36 -0800

    Add method to save form data on account page

--- public/app/account/account.controller.js
+++ public/app/account/account.controller.js
@@ -1,6 +1,14 @@
 angular.module('accountPageController', [])
   .controller('accountController', function($scope, $http) {
+    $scope.userInfo = {
+      name: 'Brian',
+      birthDate: '1/1/2017',
+      gender: 'M'
+    };
+
     $scope.saveFormData = function() {
-      console.log('yay');
+      if ($scope.formData.userName) $scope.userInfo.name = $scope.formData.userName;
+      if ($scope.formData.userBirthDate) $scope.userInfo.birthDate = $scope.formData.userBirthDate;
+      if ($scope.formData.gender) $scope.userInfo.gender = $scope.formData.gender;
     };
   });
